e8fec93bffc10113d37f325ed0ec4cfb86b90424
Leeward Bound <l@lwb.co>
2021-05-06 10:30:22 -0700

    contracts functional

--- contracts/BrainStore.sol
+++ contracts/BrainStore.sol
@@ -1,17 +1,23 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
 
-contract StoreBuilder {
-  address owner;
+uint32 constant FEE_BASIS = 10000;
+uint32 constant TOTAL_BASIS = (100 * FEE_BASIS);
+
+contract StoreBuilder is Ownable, PullPayment {
   string title;
-  uint8 defaultFeePoints;
+  uint32 defaultFeePoints;
   BrainStore[] stores;
 
-  constructor(string memory _title, uint8 _defaultFeePoints) {
+  event BrainStoreCreated(address storeAddress);
+  event Received(address, uint);
+
+  constructor(string memory _title, uint32 _defaultFeePoints) {
     console.log("Deploying a StoreBuilder");
-    owner = msg.sender;
     title = _title;
     defaultFeePoints = _defaultFeePoints;
   }
@@ -19,39 +25,101 @@ contract StoreBuilder {
   function createStore(string memory _title) public returns (address) {
     BrainStore store = new BrainStore(_title, defaultFeePoints);
     stores.push(store);
+    emit BrainStoreCreated(address(store));
+    store.transferOwnership(msg.sender);
     return address(store);
   }
 
+  function getStores() public view returns (BrainStore[] memory _stores) {
+    _stores = new BrainStore[](stores.length);
+    uint count;
+    for(uint i=0;i<stores.length; i++){
+      _stores[count] = stores[i];
+      count++;
+    }
+  }
+
+  function sweepStores() public {
+    for(uint i=0;i<stores.length; i++){
+        stores[i].sweep();
+    }
+  }
+
+  function collectFees() public {
+    for(uint i=0;i<stores.length; i++){
+      stores[i].withdrawPayments(payable(address(this)));
+    }
+  }
+
+  function transferFees() public onlyOwner {
+    _asyncTransfer(owner(), address(this).balance);
+  }
+
+  receive() external payable {
+    emit Received(msg.sender, msg.value);
+  }
 }
 
-contract BrainStore {
-  address owner;
+contract BrainStore is Ownable, PullPayment {
   string title;
-  uint8 feePoints;
+  uint32 feePoints;
   address feeProvider;
 
-  constructor(string memory _title, uint8 _feePoints) {
+  event Received(address, uint);
+
+  constructor(string memory _title, uint32 _feePoints) {
     console.log("Deploying a BrainStore", _title, _feePoints);
-    owner = msg.sender;
+    feeProvider = msg.sender;
     title = _title;
     feePoints = _feePoints;
   }
 
-  function setFee(uint8 _feePoints) public {
+  function setFee(uint32 _feePoints) public {
     require(msg.sender == feeProvider, "only the feeProvider can change the fee");
 
     console.log("Setting Store Fee:", title, _feePoints);
     feePoints = _feePoints;
   }
 
-  function getFee() public view returns (uint8 _feePoints) {
+  function getFee() public view returns (uint32 _feePoints) {
       return feePoints;
+  }
+
+  function getBalance() public view returns (uint256 _inboxBalance) {
+    return address(this).balance;
+  }
+  function getOwnerBalance() public view returns (uint256 _inboxBalance) {
+    return getBalance() - getFeeBalance();
+  }
+
+  function getFeeBalance() public view returns (uint256 _inboxFees) {
+    return getBalance() * feePoints / TOTAL_BASIS;
+  }
+
+  function sweep() public returns (uint256 _withdrawAmount) {
+    uint256 amount = getBalance();
+    _splitTransfer(amount);
+    return amount;
   }
 
   function getTitle() public view returns (string memory _title) {
       return title;
   }
 
+  function makePayment() public payable {
+    emit Received(msg.sender, msg.value);
+    _splitTransfer(msg.value);
+  }
+
+  function _splitTransfer(uint256 amount) internal {
+    uint256 feeValue = amount * feePoints / TOTAL_BASIS;
+    _asyncTransfer(feeProvider, feeValue);
+    _asyncTransfer(owner(), amount - feeValue);
+  }
+
+  receive() external payable {
+    emit Received(msg.sender, msg.value);
+  }
 }
 
 contract StoreBuilderFactory {
@@ -62,12 +130,13 @@ contract StoreBuilderFactory {
     console.log("Deploying a StoreBuilderFactory");
   }
 
-  event StoreBuilderCreated(address builderAddress, uint8 feePoints);
+  event StoreBuilderCreated(address builderAddress, uint32 feePoints);
 
-  function createStoreBuilder(string memory _title, uint8 _feePoints) public returns (address) {
+  function createStoreBuilder(string memory _title, uint32 _feePoints) public returns (address) {
     StoreBuilder builder = new StoreBuilder(_title, _feePoints);
     builders.push(builder);
     emit StoreBuilderCreated(address(builder), _feePoints);
+    builder.transferOwnership(msg.sender);
     return address(builder);
   }
 
--- test/test-factories.ts
+++ test/test-factories.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
 import { assert, expect } from "chai";
@@ -10,7 +11,7 @@ describe("Token", function () {
   });
 
   it("should have accounts", async function () {
-    assert(accounts.length > 0, "Account legth should be more then zero");
+    assert(accounts.length > 0, "Account length should be more then zero");
   });
 });
 
@@ -27,5 +28,109 @@ describe("StoreBuilderFactory", function () {
 
       await factory.deployed();
       expect(await factory.getStoreBuilders()).to.lengthOf(0)
+
+      const builderDeploy = await factory.createStoreBuilder("NFT Marketplace", 5 * 10000)
+      const builderReceipt = await builderDeploy.wait(0)
+      const makeBuilder = builderReceipt.events[2].args
+
+      expect(await factory.getStoreBuilders()).to.lengthOf(1)
+
+      const Builder = await ethers.getContractFactory("StoreBuilder")
+      const builder = await Builder.attach(makeBuilder.builderAddress)
+
+      expect(await builder.owner()).to.equal(await accounts[0].getAddress())
+      expect(5 * 10000).to.equal(makeBuilder.feePoints)
   });
+
+    it("should be able to deploy a Store", async function () {
+        const Factory = await ethers.getContractFactory("StoreBuilderFactory");
+        const factory = await Factory.deploy();
+        await factory.deployed();
+        const builderDeploy = await factory.createStoreBuilder("NFT Marketplace", 5 * 10000)
+        const builderReceipt = await builderDeploy.wait(0)
+        const makeBuilder = builderReceipt.events[2].args
+        const Builder = await ethers.getContractFactory("StoreBuilder", accounts[1])
+        const builder = await Builder.attach(makeBuilder.builderAddress)
+
+        await (await builder.createStore("My Content Creation Station")).wait(0)
+        const makeStore = (await builder.queryFilter(builder.filters.BrainStoreCreated()))[0]
+
+        const Store = await ethers.getContractFactory("BrainStore")
+        const store = await Store.attach(makeStore.args.storeAddress)
+
+        expect(await store.owner()).to.equal(await accounts[1].getAddress())
+
+    });
+
+    it("should split store payments", async function () {
+        const Factory = await ethers.getContractFactory("StoreBuilderFactory");
+        const factory = await Factory.deploy();
+        await factory.deployed();
+        const builderDeploy = await factory.createStoreBuilder("NFT Marketplace", 2 * 10000)
+        const builderReceipt = await builderDeploy.wait(0)
+        const makeBuilder = builderReceipt.events[2].args
+        const Builder = await ethers.getContractFactory("StoreBuilder", accounts[1])
+        const builder = await Builder.attach(makeBuilder.builderAddress)
+
+        expect(await builder.owner()).to.equal(await accounts[0].getAddress())
+
+        const BuilderAdmin = await ethers.getContractFactory("StoreBuilder")
+        const builderAdmin = await BuilderAdmin.attach(makeBuilder.builderAddress)
+
+        await (await builder.createStore("My Content Creation Station")).wait(0)
+        const makeStore = (await builder.queryFilter(builder.filters.BrainStoreCreated()))[0]
+
+        const Store = await ethers.getContractFactory("BrainStore")
+        const store = await Store.attach(makeStore.args.storeAddress)
+
+        expect(await store.owner()).to.equal(await accounts[1].getAddress())
+        const tx = await accounts[0].sendTransaction({
+            to: makeStore.args.storeAddress,
+            value: ethers.utils.parseEther("1.0")
+        })
+        const receipt = await tx.wait(0);
+
+        expect(await store.getOwnerBalance()).to.equal(ethers.utils.parseEther("0.98"))
+        expect(await store.getFeeBalance()).to.equal(ethers.utils.parseEther("0.02"))
+
+        expect(await store.owner()).to.equal(await accounts[1].getAddress())
+        const payment = await store.makePayment({
+            value: ethers.utils.parseEther("2.0")
+        })
+        const paystub = await tx.wait(0);
+
+        expect(await store.getOwnerBalance()).to.equal(ethers.utils.parseEther("0.98"))
+        expect(await store.getFeeBalance()).to.equal(ethers.utils.parseEther("0.02"))
+        expect(await store.payments(await accounts[1].getAddress())).to.equal(ethers.utils.parseEther("1.96"))
+        expect(await store.payments(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0.04"))
+
+        await store.sweep()
+
+        expect(await store.payments(await accounts[1].getAddress())).to.equal(ethers.utils.parseEther("2.94"))
+        expect(await store.payments(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0.06"))
+
+        await builder.collectFees()
+
+        expect(await store.payments(await accounts[1].getAddress())).to.equal(ethers.utils.parseEther("2.94"))
+        expect(await store.payments(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0"))
+        expect(await ethers.provider.getBalance(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0.06"))
+
+        const original = await accounts[0].getBalance()
+
+        await builderAdmin.transferFees()
+        await builder.withdrawPayments(await accounts[0].getAddress())
+
+        expect(await store.payments(await accounts[1].getAddress())).to.equal(ethers.utils.parseEther("2.94"))
+        expect(await store.payments(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0"))
+        expect(await ethers.provider.getBalance(makeBuilder.builderAddress)).to.equal(ethers.utils.parseEther("0.00"))
+
+        const after_fees = await accounts[0].getBalance()
+
+        const feesRecieved = parseFloat(ethers.utils.formatEther(after_fees.sub(original)))
+
+        // Expect some gas fluctuations
+        expect(feesRecieved).to.greaterThan(0.059)
+        expect(feesRecieved).to.lessThan(0.061)
+
+    });
 });
